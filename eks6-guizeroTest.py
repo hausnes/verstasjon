c1b19965d6df67e22fcc285dafe0a44da363ae0d
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-04-26 09:00:43 +0200

    GUI oppdatert jamnleg, og du kan opne fleire vindu

--- eks6-guizeroTest.py
+++ eks6-guizeroTest.py
@@ -1,4 +1,12 @@
-from guizero import App, Text
+'''
+Meir informasjon om å oppdatere GUI ved faste intervall
+https://lawsie.github.io/guizero/blocking/
+
+Meir informasjon om å bruke fleire vindu i same appen 
+https://lawsie.github.io/guizero/window/
+'''
+
+from guizero import App, Text, Window, PushButton
 import random
 
 def visData():
@@ -10,10 +18,13 @@ def hentData():
     temperatur = random.randint(5,10)
     return temperatur
 
-app = App("Temperaturvisning")
+def open_window():
+    window.show(wait=True) # Fjern 'wait=True' dersom du ynskjer å kunne bruke begge vindua samtidig
+
+app = App(title="Temperaturvisning", height=300, width=200)
+window = Window(app, title = "Vindu nr. 2", height=300, width=200)
+window.hide()
 text = Text(app, text=visData)
 text.repeat(1000, visData)  # Oppdaterer tekstfeltet sitt innhald kvar 1000ms (= 1s)
+open_button = PushButton(app, text="Opne vindu nr. 2", command=open_window)
 app.display()
-
-# Meir informasjon om å oppdatere GUI ved faste intervall
-# https://lawsie.github.io/guizero/blocking/
