ed90a9412436d10263ae80c2416b3a3b330e9d8a
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-04-26 09:42:40 +0200

    Kode for å blur-e og legge tekst på bilete.

--- eks5-blur.py
+++ eks5-blur.py
@@ -1,6 +1,22 @@
 from PIL import Image, ImageDraw, ImageFilter
+
+# Her legg me tekst over eit bilete
+# For meir informasjon: https://www.tutorialspoint.com/python_pillow/python_pillow_writing_text_on_image.htm
 innlastabilde = Image.open('bileter/ver-regn.png')
 #innlastabilde.show()
 tekstbilde = ImageDraw.Draw(innlastabilde)
 tekstbilde.text((28,36), "Jo Bjornar Hausnes", fill=(255, 0, 0))
 innlastabilde.save('bileter/temp.png')
+
+# Her gjer me eit bilete uskarpt (blur). NB: Du kan velgje mellom ein rekke teknikkar og grader av blur.
+# https://www.tutorialspoint.com/python_pillow/python_pillow_blur_an_image.htm
+originaltBilete = Image.open('bileter/ver-regn.png')
+originaltBilete.show()
+# BoxBlur filter
+boxImage = originaltBilete.filter(ImageFilter.BoxBlur(5))
+boxImage.show()
+
+#Save Boxblur image
+boxImage.save('bileter/boxblur.jpg')
+
+# No, kombiner desse to teknikkane! Blur eit bilete og legg tekst over resultatet.
