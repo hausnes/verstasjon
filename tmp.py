945f9db13d7929bcd89e1103a21c6f434ddde634
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-01-25 13:41:59 +0100

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -5,3 +5,9 @@ print(tid.date())
 import time
 no = time.strftime('%Y-%m-%d %H-%M-%S')
 print(no)
+
+cpuTemp = 54
+listeCPUTemp = [56.7,56.8,57.1,55.8,56.3]
+print(listeCPUTemp[1:])
+nyListe = listeCPUTemp[1:] + [cpuTemp]
+print(nyListe)
